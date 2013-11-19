6473e15ed8ae6d5ff9511254bd48c65fc11bbfe4
Jon Toshmatov <jht007@amfam.com>
2013-11-19 08:17:25 -0600

    added custom form functions

---
+++ custom_form_functions.js
@@ -0,0 +1,16 @@
+var TOSH = {
+		is_numeric : function(num){
+			   return typeof num === 'number' && isFinite(num);
+		},
+		//*******************************************************************//
+		is_email : function(email){
+			  var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{1,1})+$/;
+			  //var regex = /^([a-zA-Z0-9+-_.){1+}\@([a-zA-Z0-9.-_){1+}(.){1}(a-zA-Z){2+} /; acceptible 5/22
+			  return regex.test(email);
+		},
+		//*******************************************************************//
+		//get querystring by passing the querystring name for example, www.amfam.com?facebook=2232323 sVar=facebook.
+		querystring : function (sVar){
+			return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(sVar).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+		},
+}
